4e0b6948318ae77fe646203fd241ea63fb44757e
hahafunnyman <norbertgarner789@gmail.com>
2024-06-19 23:42:20 -0500

    refactor test folder

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,5 +1,5 @@
 rojo build test/default.project.json -o test/build.rbxl
 
-run-in-roblox --place test/build.rbxl --script test/src/server/runTests.server.luau
+run-in-roblox --place test/build.rbxl --script test/src/runTests.server.luau
 
 rm test/build.rbxl
